0d050959b51d777334cc2e60cb0645929f733e2d
gil-son <gilszon@gmail.com>
2021-05-03 15:47:21 -0300

    Project created

---
+++ backend/src/test/java/com/devsuperior/dssales/DssalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dssales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
