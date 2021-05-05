b92d41f389a3394f0bc18affc3b16325fa0a6201
gil-son <gilszon@gmail.com>
2021-05-05 10:38:11 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dssales/entities/Seller.java
@@ -0,0 +1,54 @@
+package com.devsuperior.dssales.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
+public class Seller {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	@OneToMany(mappedBy = "seller")
+	private List<Sale> sales = new ArrayList<>();
+	
+	public Seller() {}
+
+	public Seller(Long id, String name) {
+		// super(); Only for Only for Inheritance
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	// Just 'get' when use List
+	public List<Sale> getSales() {
+		return sales;
+	}
+
+}
